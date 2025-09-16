f27ddc128609824faadd823faa88149172bce29c
slugand <stephane.lugand@tekfor.cloud>
2025-09-16 15:27:32 +0200

    change number version

--- coreff_base/__manifest__.py
+++ coreff_base/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "CoreFF: Basic Module",
-    "version": "16.0.25.9.3",
+    "version": "16.0.1.2.0",
     "category": "CoreFF",
     "author": "Article714",
     "license": "LGPL-3",
--- coreff_creditsafe/__manifest__.py
+++ coreff_creditsafe/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "CoreFF: CreditSafe",
-    "version": "16.0.25.9.3",
+    "version": "16.0.1.2.0",
     "category": "CoreFF",
     "author": "Article714",
     "license": "LGPL-3",
