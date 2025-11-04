193c0659bcb03f569023a4b405294e2e3004fe92
Simon COLLIN <simon.collin@tekfor.cloud>
2025-11-04 11:20:14 +0100

    add _rec_name_search

--- coreff_base/models/res_partner.py
+++ coreff_base/models/res_partner.py
@@ -21,6 +21,15 @@ class ResPartner(models.Model):
         )
     ]
 
+    _rec_names_search = [
+        "complete_name",
+        "email",
+        "ref",
+        "vat",
+        "company_registry",
+        "coreff_company_code",
+    ]
+
     # CM: Add company_id field manually as required to set default
     # to current company
     company_id = fields.Many2one(
@@ -86,32 +95,11 @@ class ResPartner(models.Model):
             ):
                 raise UserError(_("Company code is required"))
 
-    @api.depends(
-        "is_company",
-        "name",
-        "parent_id.name",
-        "type",
-        "company_name",
-    )
+    @api.depends("coreff_company_code")
     def _compute_display_name(self):
-        diff = dict(
-            show_address=None,
-            show_address_only=None,
-            show_email=None,
-            html_format=None,
-        )
-        names = dict(super(ResPartner, self.with_context(**diff)).name_get())
-        for partner in self:
-            partner.display_name = names.get(partner.id)
-
-    def name_get(self):
-        res = []
-        for rec in self:
-            name = rec._get_name()
-            if rec.coreff_company_code:
-                name += f" : {rec.coreff_company_code}"
-            res.append((rec.id, name))
-        return res
+        super()._compute_display_name()
+        for partner in self.filtered("coreff_company_code"):
+            partner.display_name += f" : {partner.coreff_company_code}"
 
     # Based on https://github.com/OCA/l10n-spain/blob/16.0/l10n_es_partner/models/res_partner.py
     @api.model
@@ -121,42 +109,3 @@ class ResPartner(models.Model):
             .sudo()
             .get_param("coreff_base.name_pattern", default="")
         )
-
-    # Based on https://github.com/OCA/l10n-spain/blob/16.0/l10n_es_partner/models/res_partner.py
-    @api.model
-    def name_search(self, name, args=None, operator="ilike", limit=100):
-        """Give preference to coreff_company_code on name search, appending
-        the rest of the results after. This has to be done this way, as
-        Odoo overwrites name_search on res.partner in a non inheritable way."""
-        if (
-            "%(coreff_company_code_name)s"
-            in self._get_coreff_company_code_pattern()
-        ):
-            return super().name_search(
-                name=name, args=args, operator=operator, limit=limit
-            )
-        if not args:
-            args = []
-        partner_search_mode = self.env.context.get("res_partner_search_mode")
-        order = (
-            "{}_rank".format(partner_search_mode)
-            if partner_search_mode
-            else None
-        )
-        partners = self.search(
-            [("coreff_company_code", operator, name)] + args,
-            limit=limit,
-            order=order,
-        )
-        res = models.lazy_name_get(partners)
-        if limit:
-            limit_rest = limit - len(partners)
-        else:  # pragma: no cover
-            # limit can be 0 or None representing infinite
-            limit_rest = limit
-        if limit_rest or not limit:
-            args += [("id", "not in", partners.ids)]
-            res += super().name_search(
-                name, args=args, operator=operator, limit=limit_rest
-            )
-        return res
